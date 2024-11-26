ebe3d9f5b522c05ff510be1167b8eb3c0a5e21bd
additi123 <mehtaadditi@gmail.com>
2024-11-25 21:58:23 -0500

    changes

--- player.cc
+++ player.cc
@@ -33,18 +33,14 @@ int Player::getDownloadedViruses() const {
     return downloadedViruses;
 }
 
-Link *Player::getLink(int LinkID) const {                          // LinkID should be 0-indexed
-    return links.at(LinkID).get();
+Link &Player::getLink(int LinkID) const {                          // LinkID should be 0-indexed
+    return *(links.at(LinkID));
 }
 
-Ability *Player::getAbility(int AbilityID) const {                 // abilityID should be 0-indexed
-    return abilities.at(AbilityID).get();
+Ability &Player::getAbility(int AbilityID) const {                 // abilityID should be 0-indexed
+    return *(abilities.at(AbilityID));
 }
 
 int Player::getAbilitiesCount() const {
     return abilities.size();
-}
-
-std::vector<std::unique_ptr<Link>>* getLinks() {
-        return &links; 
 }
