ebb13b69cd7b910d5751dbee3c950a41a57b6dea
v84gupta <v84gupta@uwaterloo.ca>
2024-11-26 20:04:56 -0500

    remove one observer in main.cc

--- main.cc
+++ main.cc
@@ -62,9 +62,8 @@ int main(int argc, char* argv[]) {
 
     Game game(abilities1, abilities2, links1, links2);
     observers.emplace_back(make_unique<TextObserver>(&game, 0));
-    observers.emplace_back(make_unique<TextObserver>(&game, 1));
    // game.startGame();
-   //game.startGame();
+   // game.startGame();
    // game.initalizeLinks(game.getPlayer(0), links1);
    // game.initalizeLinks(game.getPlayer(1), links2);
    // game.initializeBoard();
