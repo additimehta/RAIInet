b349d3129930bfc4ce299d6d3e3ac5fb429c8f84
additi123 <mehtaadditi@gmail.com>
2024-11-26 19:51:18 -0500

    changes in game ctor

--- game.cc
+++ game.cc
@@ -6,9 +6,8 @@
 using namespace std;
 
 Game::Game(const string a1, const string a2, const vector<string>& links1, const vector<string>& links2) : board(std::make_unique<Board>(8, 8)), currentPlayerIndex(1), gameOver(false) {
-    
+    players.push_back(std::make_unique<Player>(0, 0, 0));
     players.push_back(std::make_unique<Player>(1, 0, 0));
-    players.push_back(std::make_unique<Player>(2, 0, 0));
     initalizeAbilites(a1, a2);
     initalizeLinks(players[0].get(), links1);
     initalizeLinks(players[1].get(), links2);
